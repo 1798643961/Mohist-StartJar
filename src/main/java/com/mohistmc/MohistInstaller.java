e7a52de6fedab31ad975dfc172ac2309a5f98df4
Administrator <1798643961@qq.com>
2020-10-04 18:40:50 +0800

    first commit

---
+++ src/main/java/com/mohistmc/MohistInstaller.java
@@ -0,0 +1,29 @@
+package com.mohistmc;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MohistInstaller {
+
+    public static void install() throws Throwable {
+
+        Path path = Paths.get(String.format("forge-1.15.2-31.2.0.jar"));
+        if (!Files.exists(path)) {
+            ExecutorService pool = Executors.newFixedThreadPool(8);
+            if (!Files.exists(path)) {
+                ProcessBuilder builder = new ProcessBuilder();
+                builder.command("java", "-jar", String.format("forge-1.15.2-31.2.0-installer.jar"), "--installServer", ".");
+                builder.inheritIO();
+                Process process = builder.start();
+                process.waitFor();
+            }
+            pool.shutdownNow();
+        }
+
+    }
+
+}
+
---
+++ src/main/java/com/mohistmc/MohistMc.java
@@ -0,0 +1,36 @@
+package com.mohistmc;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class MohistMc {
+
+    public static void main(String[] args) throws Throwable {
+
+        String path1 = String.format("forge-1.15.2-31.2.0.jar");
+
+        System.out.println("\n" + "\n" +
+                " __    __   ______   __  __   __   ______   ______  \n" +
+                "/\\ \"-./  \\ /\\  __ \\ /\\ \\_\\ \\ /\\ \\ /\\  ___\\ /\\__  _\\ \n" +
+                "\\ \\ \\-./\\ \\\\ \\ \\/\\ \\\\ \\  __ \\\\ \\ \\\\ \\___  \\\\/_/\\ \\/ \n" +
+                " \\ \\_\\ \\ \\_\\\\ \\_____\\\\ \\_\\ \\_\\\\ \\_\\\\/\\_____\\  \\ \\_\\ \n" +
+                "  \\/_/  \\/_/ \\/_____/ \\/_/\\/_/ \\/_/ \\/_____/   \\/_/ \n" +
+                "                                                    \n" + "\n");
+        afterSetup();
+        if (!Files.exists(Paths.get(path1))) {
+            System.out.println("Please reload is");
+        }
+        else {
+            ProcessBuilder builder = new ProcessBuilder();
+            builder.command("java", "-jar", String.format("forge-1.15.2-31.2.0.jar"), "nogui", ".");
+            builder.inheritIO();
+            Process process = builder.start();
+            process.waitFor();
+        }
+
+    }
+
+    protected static void afterSetup() throws Throwable {
+        MohistInstaller.install();
+    }
+}
