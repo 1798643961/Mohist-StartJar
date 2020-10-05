e8b53b6c1e54b70cd0b043986e3c9a12306d022f
Administrator <1798643961@qq.com>
2020-10-05 13:20:36 +0800

    Optimze

--- src/main/java/com/mohistmc/MohistMc.java
+++ src/main/java/com/mohistmc/MohistMc.java
@@ -1,13 +1,10 @@
 package com.mohistmc;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.File;
 
 public class MohistMc {
 
     public static void main(String[] args) throws Throwable {
-
-        String path1 = String.format("forge-1.16.3-34.1.5.jar");
 
         System.out.println("\n" + "\n" +
                 " __    __   ______   __  __   __   ______   ______  \n" +
@@ -17,17 +14,8 @@ public class MohistMc {
                 "  \\/_/  \\/_/ \\/_____/ \\/_/\\/_/ \\/_/ \\/_____/   \\/_/ \n" +
                 "                                                    \n" + "\n");
         afterSetup();
-        if (!Files.exists(Paths.get(path1))) {
-            System.out.println("Please reload it");
-        }
-        else {
-            ProcessBuilder builder = new ProcessBuilder();
-            builder.command("java", "-jar", String.format("forge-1.16.3-34.1.5.jar"), "nogui", ".");
-            builder.inheritIO();
-            Process process = builder.start();
-            process.waitFor();
-        }
-
+        MohistRuns.delete();
+        MohistRuns.runs();
     }
 
     protected static void afterSetup() throws Throwable {
